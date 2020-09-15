65ba7066b9f1e10c1a2c23fcc8d0af9db906df1d
Shyam <vnshyamkumar000@hotmail.com>
2020-09-15 19:35:18 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import picklefrom sklearn.datasets import make_regression
+import json
+
+model = pickle.load(open("models/model.pkl"),'rb')
+
+# Generate some data for validation
+X_test, y = make_regression(1000, n_features=11)
+
+# Test on that model
+y_hat = model.predict(X_test)
